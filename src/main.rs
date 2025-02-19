c776c46b165555a08bdf38361456ebdb273320e5
Anant Girdhar <anant.girdhar@proton.me>
2025-02-19 13:45:34 -0500

    Ignore results in main for the time being
    At this time, I don't have any logic for what I want to do with the
    results I get from calling the methods on library. Eventually, I'll want
    to print this stuff to the screen. But the library methods will end up
    returning different types (as I learned when I started implementing the
    searching functionality). This is not compatible with the match
    statement. I think what I want to do is implement some convenience
    methods to format the results as text and print them. So I'll just come
    back to this piece later.

--- src/main.rs
+++ src/main.rs
@@ -35,13 +35,15 @@ fn main() -> Result<()> {
     library.load()?;
     dbg!(&library);
     match args.cmd {
-        Commands::Addentry => library.add_entry(None)?,
+        Commands::Addentry => {
+            let _key = library.add_entry(None)?;
+        }
         Commands::Addfile {
             filepath,
             delete_after,
         } => {
             let filepath = PathBuf::from(&filepath);
-            library.add_file(&filepath, delete_after)?
+            let _key = library.add_file(&filepath, delete_after)?;
         }
         Commands::Searchkey => todo!("Missing implementation for searchkey"),
     };
